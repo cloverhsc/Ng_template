9884e7a4acae45c086bdaeb8045171380a983155
clover <clover@solidyear.com.tw>
2016-11-09 19:32:57 +0800

    add hero.ts and hero.gif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './hero';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'app works!';
+
+    currentHero = Hero.MockHeroes[0];
+    heroImageUrl = 'assets/hero.gif';
+    val = 2;
+
+    getVal() {
+      return this.val;
+    }
 }
